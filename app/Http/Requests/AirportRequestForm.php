e745bf24c407a1541be7a89177a026872d9bc382
John Diegor <john.diegor@gmail.com>
2018-04-17 20:42:49 +0800

    added airport index and adding of airport

---
+++ app/Http/Requests/AirportRequestForm.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Airports;
+use Illuminate\Foundation\Http\FormRequest;
+
+class AirportRequestForm extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        $input = $this->all();
+        $rules = [
+            'description' => 'string',
+            'address' => 'required|string',
+            'city' => 'required|string',
+            'county_state' => 'required|string',
+            'zipcode' => 'required',
+            'country_id' => 'required'
+        ];
+
+        if (!empty($input['id'])) {
+            $airport = Airports::find($input['id']);
+            $rules['airport_name'] = 'required|string|unique:airports,airport_name,'.$airport->id;
+        } else {
+            $rules['airport_name'] = 'required|string|unique:airports,airport_name';
+        }
+
+        return $rules;
+    }
+}
---
+++ app/Models/Tools/Countries.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Tools;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Countries extends Model
+{
+    //
+}
