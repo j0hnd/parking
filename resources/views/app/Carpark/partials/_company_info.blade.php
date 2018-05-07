efae29754d70d5978270fa249fd9c65c923174f4
John Diegor <john.diegor@gmail.com>
2018-05-07 20:24:07 +0800

    carpark's company info updates

--- resources/views/app/Carpark/partials/_company_info.blade.php
+++ resources/views/app/Carpark/partials/_company_info.blade.php
@@ -2,7 +2,7 @@
     <label class="col-sm-2 control-label">Company Name</label>
 
     <div class="col-sm-9">
-        <input type="text" class="form-control" name="company_name" placeholder="Company Name" autocomplete="off" value="">
+        <input type="text" class="form-control" name="company_name" placeholder="Company Name" autocomplete="off" value="{{ isset($carpark) ? $carpark->company->company_name : "" }}">
     </div>
 </div>
 
@@ -10,7 +10,7 @@
     <label class="col-sm-2 control-label">Email</label>
 
     <div class="col-sm-5">
-        <input type="text" class="form-control" name="email" placeholder="Email" autocomplete="off" value="">
+        <input type="text" class="form-control" name="email" placeholder="Email" autocomplete="off" value="{{ isset($carpark) ? $carpark->company->email : "" }}">
     </div>
 </div>
 
@@ -18,7 +18,7 @@
     <label class="col-sm-2 control-label">Phone No.</label>
 
     <div class="col-sm-5">
-        <input type="text" class="form-control" name="phone_no" placeholder="Phone No." autocomplete="off" value="">
+        <input type="text" class="form-control" name="phone_no" placeholder="Phone No." autocomplete="off" value="{{ isset($carpark) ? $carpark->company->phone_no : "" }}">
     </div>
 </div>
 
@@ -26,7 +26,7 @@
     <label class="col-sm-2 control-label">Mobile No.</label>
 
     <div class="col-sm-5">
-        <input type="text" class="form-control" name="mobile_no" placeholder="Mobile No." autocomplete="off" value="">
+        <input type="text" class="form-control" name="mobile_no" placeholder="Mobile No." autocomplete="off" value="{{ isset($carpark) ? $carpark->company->mobile_no : "" }}">
     </div>
 </div>
 
@@ -34,7 +34,7 @@
     <label class="col-sm-2 control-label">VAT No.</label>
 
     <div class="col-sm-5">
-        <input type="text" class="form-control" name="vat_no" placeholder="VAT No." autocomplete="off" value="">
+        <input type="text" class="form-control" name="vat_no" placeholder="VAT No." autocomplete="off" value="{{ isset($carpark) ? $carpark->company->vat_no : "" }}">
     </div>
 </div>
 
@@ -42,7 +42,7 @@
     <label class="col-sm-2 control-label">Company Registration</label>
 
     <div class="col-sm-5">
-        <input type="text" class="form-control" name="company_reg" placeholder="Company Registration" autocomplete="off" value="">
+        <input type="text" class="form-control" name="company_reg" placeholder="Company Registration" autocomplete="off" value="{{ isset($carpark) ? $carpark->company->company_reg : "" }}">
     </div>
 </div>
 
