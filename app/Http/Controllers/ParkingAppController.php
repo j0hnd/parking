db796477138ca916ea258b31cb39e6fbe82fb27b
John Diegor <john.diegor@gmail.com>
2018-05-20 11:27:05 +0800

    payment page updates

--- app/Http/Controllers/ParkingAppController.php
+++ app/Http/Controllers/ParkingAppController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Airports;
 use App\Models\Products;
 use App\Models\Tools\Common;
+use App\Models\Tools\Prices;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class ParkingAppController extends Controller
@@ -35,8 +37,33 @@ class ParkingAppController extends Controller
         return view('parking.search', compact('airports', 'time_intervals', 'results'));
     }
 
-    public function payment()
+    public function payment(Request $request)
 	{
-		return view('parking.payment');
+		if ($request->isMethod('post')) {
+			$form = $request->only(['products', 'drop_off', 'return_at']);
+
+			list($index, $product_id, $price_id, $price_value) = explode(':', $form['products']);
+			list($drop_off_date, $drop_off_time) = explode(" ", $form['drop_off']);
+			list($return_at_date, $return_at_time) = explode(" ", $form['return_at']);
+
+			$product = Products::findOrFail($product_id);
+			$price = Prices::findOrFail($price_id);
+			$booking_fee = 0;
+
+			session('products', $form['products']);
+
+			return view('parking.payment', compact(
+				'product',
+				'price',
+				'booking_fee',
+				'price_value',
+				'drop_off_date',
+				'drop_off_time',
+				'return_at_date',
+				'return_at_time'
+			));
+		}
+
+		return redirect('/');
 	}
 }
