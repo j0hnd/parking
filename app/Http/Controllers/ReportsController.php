c6c09835557c0acbe230de9ca23a35334e6c7b0d
John Diegor <john.diegor@gmail.com>
2018-06-06 18:49:12 +0800

    fixed bug in export report

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -42,11 +42,12 @@ class ReportsController extends Controller
 		return view('app.Reports.completed_jobs');
 	}
 
-	public function commissions(Request $request, Excel $excel)
+	public function commissions(Request $request)
 	{
 		$page_title = "Commissions";
 		$vendors = $this->vendors->get();
 		$bookings = null;
+		$selected_vendor = null;
 
 		if ($request->isMethod('post')) {
 			$form = $request->only(['vendor', 'date', 'export']);
@@ -62,27 +63,46 @@ class ReportsController extends Controller
 						$query->where('vendor_id', $form['vendor']);
 					})
 					->paginate(config('app.item_per_page'));
-			}
 
-			if (isset($form['export'])) {
-				$filename = "Commissions-".Carbon::now()->format('Ymd').".xlsx";
-				if (isset($form['vendor'])) {
-					$company = Companies::findOrFail($form['vendor']);
-					$filename = "Commissions-".ucwords($company->company_name)."-".Carbon::now()->format('Ymd').".xlsx";
-				}
-
-				$bookings = Bookings::active()
-					->whereRaw("DATE_FORMAT(drop_off_at, '%Y-%m-%d') >= ? AND DATE_FORMAT(return_at, '%Y-%m-%d') <= ?", [$start, $end])
-					->get();
-
-				return $excel->download(new Commissions($bookings), $filename);
+				$selected_vendor = $form['vendor'];
 			}
 		}
 
 		return view('app.Reports.commissions', [
-			'page_title' => $page_title,
-			'vendors'    => $vendors,
-			'bookings'   => $bookings
+			'page_title'      => $page_title,
+			'vendors'         => $vendors,
+			'bookings'        => $bookings,
+			'selected_vendor' => $selected_vendor
 		]);
+	}
+
+	public function export(Request $request, Excel $excel)
+	{
+		if ($request->isMethod('post')) {
+			$form = $request->only(['vendor', 'date', 'export']);
+			list($start, $end) = explode(':', $form['date']);
+
+			switch ($form['export']) {
+				case "commissions";
+					$filename = "Commissions-".Carbon::now()->format('Ymd').".xlsx";
+					$bookings = Bookings::active()
+						->whereRaw("DATE_FORMAT(drop_off_at, '%Y-%m-%d') >= ? AND DATE_FORMAT(return_at, '%Y-%m-%d') <= ?", [$start, $end])
+						->get();
+
+					if (isset($form['vendor'])) {
+						$company = Companies::findOrFail($form['vendor']);
+						$filename = "Commissions-".ucwords($company->company_name)."-".Carbon::now()->format('Ymd').".xlsx";
+						$bookings = Bookings::active()
+							->whereRaw("DATE_FORMAT(drop_off_at, '%Y-%m-%d') >= ? AND DATE_FORMAT(return_at, '%Y-%m-%d') <= ?", [$start, $end])
+							->whereHas('products', function ($query) use ($form) {
+								$query->where('vendor_id', $form['vendor']);
+							})
+							->get();
+					}
+
+					return $excel->download(new Commissions($bookings), $filename);
+					break;
+			}
+		}
 	}
 }
