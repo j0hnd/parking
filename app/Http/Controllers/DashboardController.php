e42f6b4f2bf84aeb3d1018d6e1ca8147f98e5443
John Diegor <john.diegor@gmail.com>
2018-06-07 21:19:27 +0800

    dashboard updates

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -16,28 +16,39 @@ class DashboardController extends Controller
         $now = Carbon::now();
 
         $new_bookings = Bookings::active()
-							->whereRaw("DATE_FORMAT(created_at, '%Y-%m-%d') >= ? AND DATE_FORMAT(created_at, '%Y-%m-%d') <= ?",
-								[$start->format('Y-m-d'), $now->format('Y-m-d')])
-							->count();
+			->whereRaw("DATE_FORMAT(created_at, '%Y-%m-%d') >= ? AND DATE_FORMAT(created_at, '%Y-%m-%d') <= ?",
+				[$start->format('Y-m-d'), $now->format('Y-m-d')])
+			->count();
+
         $total_bookings = Bookings::active()->count();
 
         $revenue = Bookings::active()
-					->selectRaw("SUM(revenue_value) AS revenue")
-					->whereRaw("DATE_FORMAT(created_at, '%Y-%m-%d') >= ? AND DATE_FORMAT(created_at, '%Y-%m-%d') <= ?",
-						[$start->format('Y-m-d'), $now->format('Y-m-d')])
-					->first();
+			->selectRaw("SUM(revenue_value) AS revenue")
+			->whereRaw("DATE_FORMAT(created_at, '%Y-%m-%d') >= ? AND DATE_FORMAT(created_at, '%Y-%m-%d') <= ?",
+				[$start->format('Y-m-d'), $now->format('Y-m-d')])
+			->first();
 
         $completed_jobs = Bookings::active()
-							->whereRaw("DATE_FORMAT(return_at, '%Y-%m-%d') >= ? AND DATE_FORMAT(return_at, '%Y-%m-%d') <= ?",
-								[$start->format('Y-m-d'), $now->format('Y-m-d')])
-							->count();
+			->whereRaw("DATE_FORMAT(return_at, '%Y-%m-%d') >= ? AND DATE_FORMAT(return_at, '%Y-%m-%d') <= ?",
+				[$start->format('Y-m-d'), $now->format('Y-m-d')])
+			->count();
+
+		$monthly_revenue = Bookings::selectRaw("DATE_FORMAT(bookings.created_at, '%Y-%m') AS month, SUM(bookings.price_value - bookings.revenue_value) AS revenue")
+			->whereRaw("DATE_FORMAT(bookings.created_at, '%Y-%m-%d') >= ? AND DATE_FORMAT(bookings.created_at, '%Y-%m-%d') <= ?",
+				[$start->format('Y-m-d'), $now->format('Y-m-d')])
+			->whereNull('bookings.deleted_at')
+			->join('products', 'products.id', '=', 'bookings.product_id')
+			->join('companies', 'companies.id', '=', 'products.vendor_id')
+			->groupBy(DB::raw("DATE_FORMAT(bookings.created_at, '%Y-%m')"))
+			->get();
 
         return view('app.Dashboard.index', [
-        	'page_title' => $page_title,
-			'new_bookings' => $new_bookings,
-			'total_bookings' => $total_bookings,
-			'revenue' => $revenue,
-			'completed_jobs' => $completed_jobs
+        	'page_title'       => $page_title,
+			'new_bookings'     => $new_bookings,
+			'total_bookings'   => $total_bookings,
+			'revenue' 		   => $revenue,
+			'completed_jobs'   => $completed_jobs,
+			'monthly_revenues' => $monthly_revenue
 		]);
     }
 }
