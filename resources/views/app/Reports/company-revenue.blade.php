bc6b1a4e077b856eb278db9bfd99f282d7a58740
John Diegor <john.diegor@gmail.com>
2018-06-08 21:41:08 +0800

    report updates

--- resources/views/app/Reports/company-revenue.blade.php
+++ resources/views/app/Reports/company-revenue.blade.php
@@ -1,30 +1,54 @@
 @extends('admin_template')
 
+@section('styles')
+<style type="text/css">
+	#summary td {
+		font-size: 18px;
+	}
+</style>
+@stop
+
 @section('main-content')
 	@include('app.Reports.partials._filters', ['export' => 'company_revenues', 'generate_url' => url('/admin/reports/company/revenues')])
 
 	<div class="row">
 		<div class="col-md-12">
 			<div class="table-responsive">
+				@php
+					$total = 0;
+				@endphp
 				<table class="table table-striped">
 					<thead>
 					<tr>
 						<th class="col-md-2">Vendor</th>
-						<th class="col-md-10">Revenue</th>
+						<th class="col-md-3 text-right">Revenue</th>
+						<th class="col-md-7"></th>
 					</tr>
 					</thead>
 
 					<tbody>
 					@if(count($bookings))
 						@foreach($bookings as $booking)
-							<tr id="booking-{{ $booking->id }}">
-								<td>{{ $booking->company_name }}</td>
-								<td>£{{ number_format($booking->revenue, 2) }}</td>
-							</tr>
+
+							@php
+								$total += $booking->revenue;
+							@endphp
+
+						<tr id="booking-{{ $booking->id }}">
+							<td>{{ $booking->company_name }}</td>
+							<td class="text-right">£{{ number_format($booking->revenue, 2) }}</td>
+							<td></td>
+						</tr>
 						@endforeach
+
+						<tr id="summary" class="bg-aqua">
+							<td></td>
+							<td class="text-right"><strong>£{{ number_format($total, 2) }}</strong></td>
+							<td></td>
+						</tr>
 					@else
 						<tr>
-							<td colspan="5" class="text-center"><strong>No data found</strong></td>
+							<td colspan="2" class="text-center"><strong>No data found</strong></td>
 						</tr>
 					@endif
 					</tbody>
@@ -32,7 +56,7 @@
 					@if(count($bookings))
 						<tfoot>
 						<tr>
-							<td colspan="5" class="text-right">{{ $bookings->links() }}</td>
+							<td colspan="2" class="text-right">{{ $bookings->links() }}</td>
 						</tr>
 						</tfoot>
 					@endif
