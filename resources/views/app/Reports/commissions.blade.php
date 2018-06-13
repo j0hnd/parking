4fe7fb8487eb221e73bf6e44e8b4fa40c33d0bd5
John Diegor <john.diegor@gmail.com>
2018-06-13 20:23:25 +0800

    updated for commission report

--- resources/views/app/Reports/commissions.blade.php
+++ resources/views/app/Reports/commissions.blade.php
@@ -5,6 +5,14 @@
     #summary td {
         font-size: 18px;
     }
+
+    .drop-off {
+        color: #0000ee;
+    }
+
+    .return-at {
+        color: #002a80;
+    }
 </style>
 @stop
 
@@ -15,11 +23,7 @@
         <div class="col-md-12">
             <div class="table-responsive">
                 @php
-                    $total_booking_fee = 0;
-					$total_sms = 0;
-					$total_cancellation = 0;
-					$total_share = 0;
-					$total_revenue = 0;
+                    $total_cost = 0;
                 @endphp
 
                 <table class="table table-striped">
@@ -28,43 +32,39 @@
                         <th>Booking ID</th>
                         <th>Vendor</th>
                         <th>Airport/Parking Type</th>
-                        <th class="text-right">Booking Fee</th>
-                        <th class="text-right">SMS Confirmation Fee</th>
-                        <th class="text-right">Cancellation Waiver</th>
-                        <th class="text-center">Revenue Share</th>
-                        <th class="text-right">Revenue Value</th>
+                        <th class="text-left">Customer Name</th>
+                        <th class="text-center">Book Date</th>
+                        <th class="text-center">Drop of Date/Return Date</th>
+                        <th class="text-right">Total Cost</th>
                     </tr>
                     </thead>
 
                     <tbody>
                     @if(count($bookings))
                         @foreach($bookings as $booking)
-
                             @php
-                                $total_booking_fee += $booking->booking_fees;
-                                $total_sms += $booking->sms_confirmation_fee;
-                                $total_cancellation += $booking->cancellation_waiver;
-                                $total_revenue += $booking->revenue_value;
+                                $cost = 0;
                             @endphp
-
                         <tr id="booking-{{ $booking->id }}">
-                            <td><a href="{{ url('/admin/booking/'.$booking->id.'/edit') }}">{{ $booking->booking_id }}</a></td>
+                            <td><a href="{{ url('/admin/booking/'.$booking->id.'/edit') }}" target="_blank">{{ $booking->booking_id }}</a></td>
                             <td>{{ $booking->products[0]->vendors[0]->company_name }}</td>
                             <td>{{ $booking->order_title }}</td>
-                            <td class="text-right">£{{ number_format($booking->booking_fees, 2) }}</td>
-                            <td class="text-right">{{ is_null($booking->sms_confirmation_fee) ? 0  : "£".number_format($booking->sms_confirmation_fee, 2)}}</td>
-                            <td class="text-right">{{ is_null($booking->cancellation_waiver) ? 0  : "£".number_format($booking->cancellation_waiver, 2)}}</td>
-                            <td class="text-center">{{ $booking->products[0]->revenue_share }}%</td>
-                            <td class="text-right">£{{ $booking->revenue_value + $booking->booking_fees + (is_null($booking->sms_confirmation_fee) ? 0 : $booking->sms_confirmation_fee) + (is_null($booking->cancellation_waiver) ? 0 : $booking->cancellation_waiver) }}</td>
+                            <td class="text-left">{{ $booking->customers->first_name }}&nbsp;{{ $booking->customers->last_name }}</td>
+                            <td class="text-center">{{ $booking->created_at->format('F j, Y') }}</td>
+                            <td class="text-center"><span class="drop-off">{{ $booking->drop_off_at->format('F j, Y') }}</span>/<span class="return-at">{{ $booking->return_at->format('F j, Y') }}</span></td>
+                            <td class="text-right">
+                                @php
+                                    $cost = $booking->price_value + $booking->booking_fee + $booking->sms_confirmation_fee + $booking->cancellation_waiver;
+                                    $total_cost += $cost;
+                                @endphp
+
+                                £{{ number_format($cost, 2) }}
+                            </td>
                         </tr>
                         @endforeach
                         <tr id="summary" class="bg-aqua">
-                            <td colspan="3"></td>
-                            <td class="text-right"><strong>£{{ number_format($total_booking_fee, 2) }}</strong></td>
-                            <td class="text-right"><strong>£{{ number_format($total_sms, 2) }}</strong></td>
-                            <td class="text-right"><strong>£{{ number_format($total_cancellation, 2) }}</strong></td>
-                            <td class="text-center"></td>
-                            <td class="text-right"><strong>£{{ number_format($total_revenue, 2) }}</strong></td>
+                            <td colspan="6"></td>
+                            <td class="text-right"><strong>£{{ number_format($total_cost, 2) }}</strong></td>
                         </tr>
                     @else
                         <tr>
