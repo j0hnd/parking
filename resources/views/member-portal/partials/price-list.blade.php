13af6639df511742c9dd7845ce0cad06eb58c5e1
John Diegor <john.diegor@gmail.com>
2018-09-27 21:01:07 +0800

    fixed bug in displaying override month in vendor's portal

--- resources/views/member-portal/partials/price-list.blade.php
+++ resources/views/member-portal/partials/price-list.blade.php
@@ -1,17 +1,17 @@
 @php
 	$months = [
-		1  => 'January',
-		2  => 'February',
-		3  => 'March',
-		4  => 'April',
-		5  => 'May',
-		6  => 'June',
-		7  => 'July',
-		8  => 'August',
-		9  => 'September',
-		10 => 'October',
-		11 => 'November',
-		12 => 'December',
+		'January'   => 'January',
+		'February'  => 'February',
+		'March'     => 'March',
+		'April'     => 'April',
+		'May'       => 'May',
+		'June'      => 'June',
+		'July'      => 'July',
+		'August'    => 'August',
+		'September' => 'September',
+		'October'   => 'October',
+		'November'  => 'November',
+		'December'  => 'December',
 	];
 @endphp
 
