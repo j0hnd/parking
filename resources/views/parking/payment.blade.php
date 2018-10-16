33d3040efbc6c536cdfdae1fd9d36732a0d1e84c
John Diegor <john.diegor@gmail.com>
2018-10-16 19:15:15 +0800

    corrected typo error in booking confirmation email subject

--- resources/views/parking/payment.blade.php
+++ resources/views/parking/payment.blade.php
@@ -16,6 +16,129 @@
     padding: 8px !important;
     z-index: 2002 !important;
   }
+  /* Absolute Center Spinner */
+.loading {
+  position: fixed;
+  z-index: 999999;
+  height: 2em;
+  width: 2em;
+  overflow: show;
+  margin: auto;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+}
+
+.loading p {
+  color: #ffffff;
+  font-size: 12px;
+}
+
+/* Transparent Overlay */
+.loading:before {
+  content: '';
+  display: block;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0,0,0,0.3);
+}
+
+/* :not(:required) hides these rules from IE9 and below */
+.loading:not(:required) {
+  /* hide "loading..." text */
+  font: 0/0 a;
+  color: transparent;
+  text-shadow: none;
+  background-color: transparent;
+  border: 0;
+}
+
+.loading:not(:required):after {
+  content: '';
+  display: block;
+  font-size: 10px;
+  width: 1em;
+  height: 1em;
+  margin-top: -0.5em;
+  -webkit-animation: spinner 1500ms infinite linear;
+  -moz-animation: spinner 1500ms infinite linear;
+  -ms-animation: spinner 1500ms infinite linear;
+  -o-animation: spinner 1500ms infinite linear;
+  animation: spinner 1500ms infinite linear;
+  border-radius: 0.5em;
+  -webkit-box-shadow: rgba(0, 0, 0, 0.75) 1.5em 0 0 0, rgba(0, 0, 0, 0.75) 1.1em 1.1em 0 0, rgba(0, 0, 0, 0.75) 0 1.5em 0 0, rgba(0, 0, 0, 0.75) -1.1em 1.1em 0 0, rgba(0, 0, 0, 0.5) -1.5em 0 0 0, rgba(0, 0, 0, 0.5) -1.1em -1.1em 0 0, rgba(0, 0, 0, 0.75) 0 -1.5em 0 0, rgba(0, 0, 0, 0.75) 1.1em -1.1em 0 0;
+  box-shadow: rgba(0, 0, 0, 0.75) 1.5em 0 0 0, rgba(0, 0, 0, 0.75) 1.1em 1.1em 0 0, rgba(0, 0, 0, 0.75) 0 1.5em 0 0, rgba(0, 0, 0, 0.75) -1.1em 1.1em 0 0, rgba(0, 0, 0, 0.75) -1.5em 0 0 0, rgba(0, 0, 0, 0.75) -1.1em -1.1em 0 0, rgba(0, 0, 0, 0.75) 0 -1.5em 0 0, rgba(0, 0, 0, 0.75) 1.1em -1.1em 0 0;
+}
+
+/* Animation */
+@-webkit-keyframes spinner {
+  0% {
+    -webkit-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -ms-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    -moz-transform: rotate(360deg);
+    -ms-transform: rotate(360deg);
+    -o-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+@-moz-keyframes spinner {
+  0% {
+    -webkit-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -ms-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    -moz-transform: rotate(360deg);
+    -ms-transform: rotate(360deg);
+    -o-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+@-o-keyframes spinner {
+  0% {
+    -webkit-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -ms-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    -moz-transform: rotate(360deg);
+    -ms-transform: rotate(360deg);
+    -o-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
+@keyframes spinner {
+  0% {
+    -webkit-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -ms-transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    transform: rotate(0deg);
+  }
+  100% {
+    -webkit-transform: rotate(360deg);
+    -moz-transform: rotate(360deg);
+    -ms-transform: rotate(360deg);
+    -o-transform: rotate(360deg);
+    transform: rotate(360deg);
+  }
+}
   </style>
 @stop
 
@@ -286,7 +409,7 @@
                       <div id="stripe-payment-loader" class="row d-none">
                         <div class="col-md-12 text-center">
                           <img src="{{ asset('/img/loader.gif') }}">
-                          <p>Please wait, connecting to payment gateway.</p>
+                          <p>Please wait, processing payment...</p>
                         </div>
                       </div>
                       <div id="stripe-message-wrapper" class="row d-none">
@@ -322,7 +445,7 @@
                       <div class="row">
                         @if($cancel == 1)
                           <div class="col-md-12 text-center padding-top30">
-                            <h4>Your payment has been cancelled</h4>
+                            <h4>Oops! Something went wrong while processing your payment or you opt to cancel.<br/>Thus, you will not be able to continue booking this carpark.</h4>
                           </div>
                         @else
                           <div class="col-md-12 text-center padding-top30">
@@ -627,6 +750,10 @@
             </div>
           </div>
         </div>
+
+        <div class="loading">
+          <p>Please wait while we process your payment...</p>
+        </div>
       </main>
     @stop
 
@@ -658,11 +785,11 @@
           $('#other-vehicle-model-src').removeClass('d-none');
         }
 
-        if (payment_confirm == 0 && cancel == 0) {
+        if ((payment_confirm == 0 && cancel == 0) || (payment_confirm == 1 && cancel == 1)) {
           setTimeout(function () {
             $("a[href='#next']").parent().addClass("disabled").attr("aria-disabled", "true");
-            $('div.actions ul li:nth-child(1)').attr('href', '#');
-            $('div.actions ul li:nth-child(2)').attr('href', '#');
+            $('div.actions ul li:nth-child(1) a').attr('href', 'javascript:void(0)');
+            $('div.actions ul li:nth-child(2) a').attr('href', 'javascript:void(0)');
           }, 300);
         }
 
@@ -718,14 +845,15 @@
                   data: $('#order-form').serialize(),
                   dataType: 'json',
                   cache: false,
-                  async: false,
                   beforeSend: function () {
                       $('#payment_choice').find('#stripe-payment-loader').removeClass('d-none');
                       $('#payment_choice').find('#stripe-container').addClass('d-none');
+                      $('.loading').removeClass('d-none');
                   },
                   success: function (response) {
                       $('#payment_choice').find('#stripe-payment-loader').addClass('d-none');
                       $('#payment_choice').find('#stripe-container').removeClass('d-none');
+                      $('.loading').addClass('d-none');
 
                       if (response.success) {
                           $('#payment_choice').find('#stripe-container').addClass('d-none');
